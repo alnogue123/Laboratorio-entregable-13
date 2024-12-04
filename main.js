66ad285b5baa47c2ac81f2216e23bc07f7c80269
alnogue123 <alnogue18@gmail.com>
2024-12-04 09:16:21 -0500

    Entrega final Calculadora en js

--- main.js
+++ main.js
@@ -1,30 +1,44 @@
-let Operacion = prompt("ingresa la operacion que quieres hacer" +
-    "\n 1: Suma \n 2: Resta" +
-    "\n 3: Multiplicar \n 4: dividir"
-);
+let encurso = true;
+let numero1;
+let numero2;
 
-let numero1 = parseInt(prompt("Ingrese el primer numero"));
-let numero2 = parseInt(prompt("Ingrese el segundo numero"));
-
-RealizarCalculos(numero1, numero2, Operacion);
+while (encurso) {
+    MenuUsuario();
+}
 
 function RealizarCalculos(numero1, numero2, Operacion) {
     switch (Operacion) {
         case "1":
-            alert(`El resultado es de ${Suma(numero1,numero2)}`);
+            alert(`El resultado es de ${Suma(numero1, numero2)}`);
             break;
         case "2":
-            alert(`El resultado es de ${Resta(numero1,numero2)}`);
+            alert(`El resultado es de ${Resta(numero1, numero2)}`);
             break;
         case "3":
-            alert(`El resultado es de ${Multiplicar(numero1,numero2)}`);
+            alert(`El resultado es de ${Multiplicar(numero1, numero2)}`);
             break;
         case "4":
-            alert(`El resultado es de ${Dividir(numero1,numero2)}`);
+            alert(`El resultado es de ${Dividir(numero1, numero2)}`);
             break;
         default:
             console.log("operacion incorrecta");
             break;
+    }
+}
+
+function MenuUsuario() {
+    let Operacion = prompt("ingresa la operacion que quieres hacer" +
+        "\n 1: Suma \n 2: Resta" +
+        "\n 3: Multiplicar \n 4: Dividir \n 5: Salir"
+    );
+
+    if (Operacion != "5") {
+        numero1 = parseInt(prompt("Ingrese el primer numero"));
+        numero2 = parseInt(prompt("Ingrese el segundo numero"));
+        RealizarCalculos(numero1, numero2, Operacion);
+    } else {
+        encurso = false;
+        alert("Terminando el programa...");
     }
 }
 
